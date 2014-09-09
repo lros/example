61a84284d20ba4337993462fa319fde5aa8161e3
Steve Clark <sclark724@gmail.com>
2014-09-09 17:04:35 -0400

    Trying to get timed_wait() to work.

--- test4/test4.cpp
+++ test4/test4.cpp
@@ -1,46 +1,66 @@
+// TODO include specific subsets
 #include <boost/thread.hpp>
 #include <boost/date_time.hpp>
-#include <stdio.h>
+#include <iostream>
 
-boost::condition_variable ping;
-boost::mutex mx;
-bool got_data;
-unsigned value;
+boost::condition_variable gCond;
+boost::mutex gMutex;
+bool gGotData;
+unsigned gValue;
 
 void threadfn() {
-    for (unsigned i = 0; i < 10; i++) {
-        printf("hello from threadfn() #%d\n", i);
+    for (unsigned i = 0; i < 7; i++) {
+        std::cerr << "hello from threadfn(), #" << i << std::endl;
         {
-            boost::lock_guard<boost::mutex> guard(mx);
-            got_data = true;
-            value = i;
+            boost::lock_guard<boost::mutex> guard(gMutex);
+            gGotData = true;
+            gValue = i;
         }
-        ping.notify_one();
+        gCond.notify_one();
         boost::this_thread::sleep(boost::posix_time::seconds(1));
     }
 }
 
+//bool 
+
+#define WAIT
+//#define TIMED_WAIT
+
 int main() {
-    got_data = false;
-    value = 999;
-    printf("main(), before launching.\n");
+    gGotData = false;
+    gValue = 999;
     boost::thread t(threadfn);
-    printf("main(), after launching.\n");
-    bool timedOut = false;
-    while (!timedOut) {
-        boost::posix_time::ptime deadline(
-                boost::posix_time::microsec_clock::local_time()
-                    + boost::posix_time::seconds(2));
-        boost::unique_lock<boost::mutex> guard(mx);
-        while(!got_data && !timedOut) {
-            timedOut = ! ping.timed_wait(guard, deadline);
+    while (true) {
+        boost::system_time deadline(
+                boost::posix_time::microsec_clock::local_time());
+        std::cerr << "now is " << deadline << std::endl;
+        deadline += boost::posix_time::seconds(2);
+        std::cerr << "deadline is " << deadline << std::endl;
+        boost::unique_lock<boost::mutex> lock(gMutex);
+        unsigned nWakeups = 0;
+        while(!gGotData) {
+            //timedOut = ! gCond.timed_wait(lock, deadline);
+            //std::cerr << "main(), gCond.wait() returned, gGotData is "
+                    //<< gGotData << std::endl;
+#if defined TIMED_WAIT
+            bool ret = gCond.timed_wait(lock, deadline);
+            nWakeups++;
+            if (ret) std::cerr << "timed_wait() returned true on wakeup #"
+                    << nWakeups << std::endl;
+#elif defined WAIT
+            gCond.wait(lock);
+            nWakeups++;
+#endif
         }
-        if (!timedOut) {
-            printf("main(), pinged, value is %d\n", value);
-        } else {
-            printf("main(), timed out\n");
+        std::cerr << "main() exited inner loop with " << nWakeups
+                << " wakeups; gGotData is "
+                << (gGotData ? "true" : "false") << std::endl;
+        if (gGotData) {
+            std::cerr << "main(), notified, gValue is " << gValue << std::endl;
+            gGotData = false;
         }
     }
+    // not reached in this incarnation of the code
     t.join();
-    printf("main(), after joining.\n");
+    std::cerr << "main(), after joining" << std::endl;
 }
